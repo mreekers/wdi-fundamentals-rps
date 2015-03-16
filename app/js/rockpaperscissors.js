6ae23a464627a86b21e4b7d5e46e5b583c26faee
Michael Reekers <mreekers@Michaels-MacBook-Air.local>
2015-03-15 19:03:08 -0700

    session 3 code changes

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,7 +25,8 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return return var move = "some_value":
+            move = move || getInput();
 }
 
 function getComputerMove(move) {
